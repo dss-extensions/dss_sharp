d4029b28b8a9d434824d27d0672729adf340fec0
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2023-05-25 15:40:26 -0300

    Prepare version 0.13.2 and update CI config.

--- scripts/ci_update_versions.sh
+++ scripts/ci_update_versions.sh
@@ -2,7 +2,7 @@
 sed -i -r 's/(<FileVersion>[0-9]+\.[0-9]+\.[0-9]+\.)[0-9]+/\1$(GITHUB_RUN_NUMBER)/' dss_sharp.csproj
 
 if [ "tag" == "${GITHUB_REF_TYPE}" ]; then
-    sed -i -r 's/<PackageVersion>.*<\/PackageVersion>/<PackageVersion>$(GITHUB_REF)<\/PackageVersion>/' dss_sharp.csproj
+    sed -i -r 's/<PackageVersion>.*<\/PackageVersion>/<PackageVersion>$(GITHUB_REF_NAME)<\/PackageVersion>/' dss_sharp.csproj
 fi
 
 cat dss_sharp.csproj
--- scripts/download_native_libs.sh
+++ scripts/download_native_libs.sh
@@ -1,5 +1,5 @@
 if [ -z ${DSS_CAPI_TAG+x} ]; then
-    DSS_CAPI_TAG=0.13.1
+    DSS_CAPI_TAG=0.13.2
 fi
 
 rm -rf runtimes messages
